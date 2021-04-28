0175d876edae0938640fb00077e84789c760cbff
thecodingking10 <67853976+thecodingking10@users.noreply.github.com>
2021-04-28 16:55:16 +0530

    uploaded

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 700,150)
+    fill("lightblue")
+    text("Hint: Stores All Information !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,250)
+    fill("lightblue")
+    text("Hint: Performs a Particular Task !!", 100,270)
+// add code for displaying rest of the names and the hints.
+    
+
+}
